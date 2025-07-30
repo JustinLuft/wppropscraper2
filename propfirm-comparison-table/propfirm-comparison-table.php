187298c80d1daa9b74d7a2071ded1cd883009c55
Justin <89544777+JustinLuft@users.noreply.github.com>
2025-07-29 21:32:12 -0400

    Update propfirm-comparison-table.php

--- propfirm-comparison-table/propfirm-comparison-table.php
+++ propfirm-comparison-table/propfirm-comparison-table.php
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 // Define plugin constants
-define('PFCT_VERSION', '1.2');
+define('PFCT_VERSION', '2.0');
 define('PFCT_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('PFCT_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('PFCT_PLUGIN_BASENAME', plugin_basename(__FILE__));
